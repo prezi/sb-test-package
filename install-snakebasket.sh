6144f78a13550a89d1012ee84e9bfd1ad04286b3
Peter Neumark <peter.neumark@prezi.com>
2012-12-01 15:37:03 +0100

    Make sure my changes dont break other builds

--- install-snakebasket.sh
+++ install-snakebasket.sh
@@ -1,2 +1,2 @@
-pip install -e git+http://github.com/pypa/pip.git@854574f65c4ef92da8dd78b9831a9604dcc9957a#egg=pip -e git+git@github.com:prezi/snakebasket.git#egg=snakebasket
+pip install -e git+http://github.com/pypa/pip.git@854574f65c4ef92da8dd78b9831a9604dcc9957a#egg=pip -e git+git@github.com:prezi/snakebasket.git@3806a18e0ed766263de5b2dd26c9033af3a202ea#egg=snakebasket
 
