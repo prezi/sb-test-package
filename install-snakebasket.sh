6c6063eea8e2c2c36e7deb7665e5b552a4d8945a
Peter Neumark <peter.neumark@prezi.com>
2012-12-19 17:06:25 +0100

    Update install-snakebasket.sh

--- install-snakebasket.sh
+++ install-snakebasket.sh
@@ -1,2 +1,2 @@
-pip install -e git+http://github.com/pypa/pip.git@854574f65c4ef92da8dd78b9831a9604dcc9957a#egg=pip -e git+git@github.com:prezi/snakebasket.git@72debeeeacdeb5f0ba775c0f30e8954e9caf55a6#egg=snakebasket
+pip install -e git+http://github.com/pypa/pip.git@854574f65c4ef92da8dd78b9831a9604dcc9957a#egg=pip -e git+git@github.com:prezi/snakebasket.git@2dc61f3007e2454f9d0d0139aaffb11fc3cd1250#egg=snakebasket
 
