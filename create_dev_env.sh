7214f1fd5cdbe71dee4464894333cd21738bf761
Peter Neumark <peter.neumark@prezi.com>
2013-01-03 17:01:13 +0100

    added create_dev_env.sh to master branch

---
+++ create_dev_env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+appname=${1:?App name mandatory}
+# Create virtualenv
+virtualenv --distribute $appname
+# Activate the virtualenv
+. $appname/bin/activate 
+# Install snakebasket and then checkout the project
+curl -ss -L http://href.prezi.com/snakebasket | bash -s 
+# Install the project in the virtualenv
+projecturl=git@github.com:prezi/$appname.git
+sb install --env local -e git+$projecturl#egg=$appname
