32502ba8dbf321b35d705a3654508419a2aa7898
dtorok <daniel.torok@prezi.com>
2013-01-22 12:35:03 +0100

    Update install-snakebasket.sh
    snakebasket bump

--- install-snakebasket.sh
+++ install-snakebasket.sh
@@ -1,1 +1,1 @@
-pip install -e git+http://github.com/pypa/pip.git@854574f65c4ef92da8dd78b9831a9604dcc9957a#egg=pip -e git+git@github.com:prezi/snakebasket.git@2dc61f3007e2454f9d0d0139aaffb11fc3cd1250#egg=snakebasket
+pip install -e git+http://github.com/pypa/pip.git@854574f65c4ef92da8dd78b9831a9604dcc9957a#egg=pip -e git+git@github.com:prezi/snakebasket.git@fc83fd0d2ebc2f3f32f484f2b9ba076278b5ea57#egg=snakebasket
