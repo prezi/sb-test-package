13da85c1926efc84b76ebc809efb0cda709bc40f
Peter Neumark <peter.neumark@prezi.com>
2013-04-18 18:32:26 +0300

    bump

--- install-snakebasket.sh
+++ install-snakebasket.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pip install -e git+http://github.com/pypa/pip.git@854574f65c4ef92da8dd78b9831a9604dcc9957a#egg=pip -e git+git@github.com:prezi/snakebasket.git@04d88a54b725fb61aa42ef355291e73eedc6033b#egg=snakebasket
+pip install -e git+http://github.com/pypa/pip.git@854574f65c4ef92da8dd78b9831a9604dcc9957a#egg=pip -e git+git@github.com:prezi/snakebasket.git@b52f5819e88597a5d4b38b4671a2c68a3e0460b2#egg=snakebasket
