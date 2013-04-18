b3edd5cab51f98dab6f00ee487acba201a5198be
Ryan Lane <ryan@ryanalane.com>
2013-04-18 16:52:27 +0300

    Bumped version of snakebasket

--- install-snakebasket.sh
+++ install-snakebasket.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pip install -e git+http://github.com/pypa/pip.git@854574f65c4ef92da8dd78b9831a9604dcc9957a#egg=pip -e git+git@github.com:prezi/snakebasket.git@38f9de483fabd678cf410b7f8478c4fe7cdc1817#egg=snakebasket
+pip install -e git+http://github.com/pypa/pip.git@854574f65c4ef92da8dd78b9831a9604dcc9957a#egg=pip -e git+git@github.com:prezi/snakebasket.git@ab6e88ae4c247b29e30dc6920f4775c656455f5d#egg=snakebasket
