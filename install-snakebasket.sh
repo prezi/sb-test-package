d24b2f20fe002ba792ab8df7040d043215e2021a
Peter Neumark <peter.neumark@prezi.com>
2013-04-19 12:26:24 +0200

    install-snakebasket script now checks for virtualenv

--- install-snakebasket.sh
+++ install-snakebasket.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-pip install -e git+http://github.com/pypa/pip.git@854574f65c4ef92da8dd78b9831a9604dcc9957a#egg=pip -e git+git@github.com:prezi/snakebasket.git@b52f5819e88597a5d4b38b4671a2c68a3e0460b2#egg=snakebasket
+PIP_VERSION=854574f65c4ef92da8dd78b9831a9604dcc9957a
+SB_VERSION=b52f5819e88597a5d4b38b4671a2c68a3e0460b2
+
+not_in_env_error() {
+    echo "You are not currently in a python virtualenv."
+    echo "Please activate a virtualenv to install snakebasket."
+    exit 1
+}
+[ -z "${VIRTUAL_ENV:-}" ] && not_in_env_error
+pip install -e git+http://github.com/pypa/pip.git@$PIP_VERSION#egg=pip -e git+git@github.com:prezi/snakebasket.git@$SB_VERSION#egg=snakebasket
